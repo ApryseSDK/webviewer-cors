32026a3c63fd5c302824edf6b09dc8f335a07fcf
Andrey <a.so777@live.ca>
2020-08-08 18:33:54 -0700

    Added postMessage and loading of the document

--- webviewer-app/src/App.tsx
+++ webviewer-app/src/App.tsx
@@ -9,10 +9,19 @@ function App() {
     WebViewer(
       {
         path: 'http://localhost:8080/lib/',
-        config: 'http://localhost:8080/config.js'
+        config: 'http://localhost:8080/config.js',
       },
       viewer.current as HTMLDivElement,
     );
+    
+    viewer?.current?.addEventListener('ready', () => {
+      const iframeWindow = viewer?.current?.querySelector('iframe')?.contentWindow;
+      setTimeout(() => {
+        iframeWindow?.postMessage({ 
+        type: 'OPEN_DOCUMENT_URL', 
+        url: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf' 
+      }, '*')}, 1000);
+    });
   }, []);
 
   return (
